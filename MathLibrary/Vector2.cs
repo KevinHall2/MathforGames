f2333c33ff908b82516a5e6d6013b8071f9086bb
KevinHall2 <khjiitech@gmail.com>
2024-10-11 10:10:17 -0500

    Add project files.

---
+++ MathLibrary/Vector2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLibrary
+{
+    public class Vector2
+    {
+        private float x;
+        private float y;
+
+
+        public Vector2()
+        {
+            x = 0;
+            y = 0;
+        }
+
+
+        public Vector2(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        //operator overload for addition
+        public static Vector2 operator +(Vector2 left, Vector2 right)
+        {
+            return new Vector2(left.x + right.x, left.y + right.y);
+        }
+
+        //operator overload for subtraction
+        public static Vector2 operator -(Vector2 left, Vector2 right)
+        {
+            return new Vector2(left.x - right.x, left.y - right.y);
+        }
+
+        //operator overload for multiplication by a vector
+        public static Vector2 operator *(Vector2 left, Vector2 right)
+        {
+            return new Vector2(left.x * right.x, left.y * right.y);
+        }
+
+        //operator overload for multiplication by a scalar
+        public static Vector2 operator *(Vector2 left, float right)
+        {
+            return new Vector2(left.x * right, left.y * right);
+        }
+
+        //operator overload for division
+        public static Vector2 operator /(Vector2 left, Vector2 right)
+        {
+            return new Vector2(left.x / right.x, left.y / right.y);
+        }
+    }
+}
---
+++ Sandbox/Program.cs
@@ -0,0 +1,30 @@
+﻿using MathLibrary;
+using Raylib_cs;
+using System.Numerics;
+
+namespace Sandbox
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.InitWindow(800, 450, "raylib [core] example - basic window");
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.Gray);
+                Raylib.DrawRectangle(400, 225, 75, 75, Color.White);
+                if (Raylib.IsKeyDown(KeyboardKey.W))
+                {
+                    
+                }
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
+
+            return;
+        }
+    }
+}
